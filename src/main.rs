77bc30ff033f3acb4e4c04455f79e990876ded57
Thomas Hurst <tom@hur.st>
2018-05-09 22:25:56 +0100

    Initial pass at different bundled dictionaries.
    Needs a fair bit of tidying.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,54 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
+use std::path::PathBuf;
 
 extern crate rand;
 use rand::distributions::{IndependentSample, Range};
 
+#[macro_use]
 extern crate failure;
 use failure::{Error, ResultExt};
 
 #[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
 extern crate structopt;
 
-use std::path::PathBuf;
 use structopt::StructOpt;
 
-fn sample_dict<'a>(mut rng: &mut rand::OsRng, dict: &'a [&str], samples: usize) -> Vec<&'a str> {
-    let range = Range::new(0, dict.len());
+struct PassFormat {
+    data: &'static str,
+    separator: String,
+}
 
-    let mut ret: Vec<&str> = Vec::new();
+macro_rules! defdict {
+    ($map:expr, $name:expr, $separator:expr) => {
+        $map.insert(
+            $name,
+            PassFormat {
+                data: include_str!(concat!("../dictionaries/", $name, ".txt")),
+                separator: $separator.to_owned(),
+            },
+        );
+    };
+}
 
-    for _ in 0..samples {
-        ret.push(dict[range.ind_sample(&mut rng)]);
-    }
-
-    ret
+lazy_static! {
+    static ref DICTIONARIES: HashMap<&'static str, PassFormat> = {
+        let mut m = HashMap::new();
+        defdict!(m, "eff", " ");
+        defdict!(m, "alpha", "");
+        defdict!(m, "mixedalpha", "");
+        defdict!(m, "mixedalphanumeric", "");
+        defdict!(m, "alphanumeric", "");
+        defdict!(m, "pin", "");
+        defdict!(m, "hex", "");
+        defdict!(m, "printable", "");
+        defdict!(m, "koremutake", "");
+        m
+    };
 }
 
 #[derive(Debug, StructOpt)]
@@ -33,8 +59,8 @@ struct Opt {
     verbose: bool,
 
     /// Word separator
-    #[structopt(short = "s", long = "separator", default_value = " ")]
-    separator: String,
+    #[structopt(short = "s", long = "separator")]
+    separator: Option<String>,
 
     /// Number of passwords to generate
     #[structopt(short = "n", long = "number", default_value = "1")]
@@ -48,9 +74,26 @@ struct Opt {
     #[structopt(short = "l", long = "length")]
     length: Option<u32>,
 
-    /// Dictionary to use (default: built-in EFF Diceware)
+    /// External dictionary
     #[structopt(short = "w", long = "wordlist", parse(from_os_str))]
     wordlist: Option<PathBuf>,
+
+    /// Built-in dictionary [default: eff]
+    #[structopt(short = "d", long = "dictionary",
+                raw(possible_values = "&DICTIONARIES.keys().map(|s| *s).collect::<Vec<&str>>()"))]
+    dict: Option<String>,
+}
+
+fn sample_dict<'a>(mut rng: &mut rand::OsRng, dict: &'a [&str], samples: usize) -> Vec<&'a str> {
+    let range = Range::new(0, dict.len());
+
+    let mut ret: Vec<&str> = Vec::new();
+
+    for _ in 0..samples {
+        ret.push(dict[range.ind_sample(&mut rng)]);
+    }
+
+    ret
 }
 
 fn run() -> Result<(), Error> {
@@ -58,6 +101,7 @@ fn run() -> Result<(), Error> {
     let eff = include_str!("../eff.txt");
     let mut wordlist = String::new();
     let mut dict: Vec<&str>;
+    let mut separator = " ".to_owned();
 
     if let Some(wl) = opts.wordlist {
         let mut inf = File::open(&wl).with_context(|e| format!("{}: {}", &wl.display(), e))?;
@@ -70,35 +114,38 @@ fn run() -> Result<(), Error> {
             .collect();
         dict.sort_unstable();
         dict.dedup();
+    } else if let Some(d) = opts.dict {
+        if let Some(dd) = DICTIONARIES.get(&d[..]) {
+            dict = dd.data.lines().collect();
+            separator = dd.separator.clone();
+        } else {
+            bail!(
+                "Valid dictionaries: {:?}",
+                DICTIONARIES.keys().collect::<Vec<&&str>>()
+            );
+        }
     } else {
         dict = eff.lines().collect();
     }
 
-    let length;
-    let bits;
-    if let Some(l) = opts.length {
-        length = l;
-    } else {
-        length = (opts.bits / (dict.len() as f64).log2()).ceil() as u32;
-    }
-
-    let combinations = (dict.len() as f64).powf(f64::from(length));
-    bits = (combinations).log2();
-
-    let mut rng = rand::OsRng::new().expect("Failed to open RNG");
+    let length = opts.length
+        .unwrap_or((opts.bits / (dict.len() as f64).log2()).ceil() as u32);
 
     if opts.verbose {
+        let combinations = (dict.len() as f64).powf(f64::from(length));
         println!(
             "# Complexity {}^{}={:.0}, {:.2} bits of entropy",
             dict.len(),
             length,
             combinations,
-            bits
+            combinations.log2()
         );
     }
+
+    let mut rng = rand::OsRng::new().expect("Failed to open RNG");
     for _ in 0..opts.number {
         let pw = sample_dict(&mut rng, &dict, length as usize);
-        println!("{}", pw.join(&opts.separator));
+        println!("{}", pw.join(&separator));
     }
 
     Ok(())
